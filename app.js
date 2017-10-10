47992f9fa187d27fe908027f1b6a841ecba57243
Dana Woodman <dana@danawoodman.com>
2017-10-10 15:59:25 -0700

    Update api domain

--- app.js
+++ app.js
@@ -113,20 +113,20 @@ function Row({ columns, item }) {
 }
 
 ReactDOM.render(
-  <SmartTable url="http://app.tubbsfireinfo.com/recent_news.json" />,
+  <SmartTable url="http://app.sonomafireinfo.com/recent_news.json" />,
   document.getElementById('resource-table')
 )
 ReactDOM.render(
-  <SmartTable url="http://app.tubbsfireinfo.com/gas_stations.json" />,
+  <SmartTable url="http://app.sonomafireinfo.com/gas_stations.json" />,
   document.getElementById('gas-stations-table')
 )
 
 ReactDOM.render(
-  <SmartTable url="http://app.tubbsfireinfo.com/markets.json" />,
+  <SmartTable url="http://app.sonomafireinfo.com/markets.json" />,
   document.getElementById('markets-table')
 )
 
 ReactDOM.render(
-  <SmartTable url="http://app.tubbsfireinfo.com/shelters.json" />,
+  <SmartTable url="http://app.sonomafireinfo.com/shelters.json" />,
   document.getElementById('shelters-table')
 )
--- build.js
+++ build.js
@@ -10040,12 +10040,12 @@ function Row({ columns, item }) {
   );
 }
 
-ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.tubbsfireinfo.com/recent_news.json' }), document.getElementById('resource-table'));
-ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.tubbsfireinfo.com/gas_stations.json' }), document.getElementById('gas-stations-table'));
+ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.sonomafireinfo.com/recent_news.json' }), document.getElementById('resource-table'));
+ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.sonomafireinfo.com/gas_stations.json' }), document.getElementById('gas-stations-table'));
 
-ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.tubbsfireinfo.com/markets.json' }), document.getElementById('markets-table'));
+ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.sonomafireinfo.com/markets.json' }), document.getElementById('markets-table'));
 
-ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.tubbsfireinfo.com/shelters.json' }), document.getElementById('shelters-table'));
+ReactDOM.render(React.createElement(SmartTable, { url: 'http://app.sonomafireinfo.com/shelters.json' }), document.getElementById('shelters-table'));
 
 /***/ }),
 /* 343 */
