625182176756fd6e85170e8f3980da016140858a
Abhi Yerra <abhi@berkeley.edu>
2017-10-10 21:17:01 +0000

    rails caching

--- FireResponse/app/controllers/tables_controller.rb
+++ FireResponse/app/controllers/tables_controller.rb
@@ -2,9 +2,11 @@ class TablesController < ApplicationController
 
   AirtableSheets.each do |k,v|
     define_method(k) do
-      ::Rails.cache.fetch("cache/#{k}", expires_in: 30.seconds) do
-        render json: v.all.to_json
+      records = ::Rails.cache.fetch("cache/#{k}", expires_in: 30.seconds) do
+         v.all.to_json
       end
+
+      render json: records
     end
   end
 end
