91d64be3537d3a33f67f0f848d467ef0eb8277bb
Dana Woodman <dana@danawoodman.com>
2017-10-11 15:25:43 -0700

    Fix phone link text

--- phone-link.js
+++ phone-link.js
@@ -5,7 +5,7 @@ function PhoneLink({ number }) {
   const cleaned = new PhoneNumber(number, 'US')
   return (
     <a href={`tel:1${cleaned.getNumber('significant')}`}>
-      {cleaned.getNumber()}
+      {cleaned.getNumber('significant')}
     </a>
   )
 }
