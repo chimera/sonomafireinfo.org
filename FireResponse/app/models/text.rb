41b138992a761f49d0044a5c9fad3d4edd52fb9d
Abhi Yerra <abhi@berkeley.edu>
2017-10-12 23:52:55 +0000

    logger

--- FireResponse/app/models/text.rb
+++ FireResponse/app/models/text.rb
@@ -8,14 +8,14 @@ class Text
     client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     numbers.each do |number|
       begin
+        Rails.logger.info "Send message to #{number}"
         client.api.account.messages.create(
           from: '+17078004983',
           to: "+1#{number}",
           body: "SonomaFireInfo.com: #{msg}"
         )
-        Rails.logger.info "Send message to #{number}"
       rescue Exception => e
-        puts "Failed #{e}"
+        Rails.logger.error "Failed #{e}"
       end
     end
   end
