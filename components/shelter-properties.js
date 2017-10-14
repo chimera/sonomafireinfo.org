5134c93aff9be6168baa2bd23664c6c8c126c505
Dana Woodman <dana@danawoodman.com>
2017-10-14 13:33:18 -0700

    Show shelter properties again

--- components/shelter-properties.js
+++ components/shelter-properties.js
@@ -14,34 +14,34 @@ export default function ShelterProperties({ item }) {
   const spanish = item.hablanEspanol
   const redCross = item.redCrossFacility
 
-  if (!capacity || !elder || !spanish || !redCross) {
-    return null
+  if (capacity || elder || spanish || redCross) {
+    return (
+      <div className="mt-2">
+        <small>
+          {capacity && (
+            <strong className="mr-3 text-danger" title="At capacity">
+              <span className="mr-2">❌</span> At Capacity
+            </strong>
+          )}
+          {elder && (
+            <span className="mr-3" title="Has elder care">
+              <span className="mr-2">👵</span> Elder Care
+            </span>
+          )}
+          {spanish && (
+            <span className="mr-3" title="Hablan Español">
+              <span className="mr-2">🇲🇽</span> Hablan Español
+            </span>
+          )}
+          {redCross && (
+            <span className="mr-3" title="Red Cross Facility">
+              <span className="mr-2">🏥</span> Red Cross Facility
+            </span>
+          )}
+        </small>
+      </div>
+    )
   }
 
-  return (
-    <div className="mt-2">
-      <small>
-        {capacity && (
-          <span className="mr-3" title="At capacity">
-            <span className="mr-2">❌</span> At Capacity
-          </span>
-        )}
-        {elder && (
-          <span className="mr-3" title="Has elder care">
-            <span className="mr-2">👵</span> Elder Care
-          </span>
-        )}
-        {spanish && (
-          <span className="mr-3" title="Hablan Español">
-            <span className="mr-2">🇲🇽</span> Hablan Español
-          </span>
-        )}
-        {redCross && (
-          <span className="mr-3" title="Red Cross Facility">
-            <span className="mr-2">🏥</span> Red Cross Facility
-          </span>
-        )}
-      </small>
-    </div>
-  )
+  return null
 }
