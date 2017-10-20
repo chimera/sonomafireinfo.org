5b1a4752165b68715e7ae53cba22e8adc5176504
Dana Woodman <dana@danawoodman.com>
2017-10-20 09:22:46 +0700

    Make label optional for note

--- components/note.js
+++ components/note.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 Note.defaultProps = {}
 
 Note.propTypes = {
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
   note: PropTypes.string.isRequired,
 }
 
@@ -12,7 +12,7 @@ export default function Note({ label, note }) {
   return (
     <div className="mt-2 text-muted">
       <small>
-        <strong className="mr-2 text-uppercase">{label}:</strong>
+        {label && <strong className="mr-2 text-uppercase">{label}:</strong>}
         {note}
       </small>
     </div>
